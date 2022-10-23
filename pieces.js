997b791518e1b7850ba57775f4462c4ebab6847e
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-23 18:21:54 -0500

    Add exercice code

--- pieces.js
+++ pieces.js
@@ -11,9 +11,21 @@ const prixElement = document.createElement("p");
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+
+//Création des élements pour l'exercice
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
+
+const stockElement = document.createElement("p");
+stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+
+//Ajout des éléments au DOM pour l'exercice
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(stockElement);
