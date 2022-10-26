69c28c7415effef60737e1317565c9ce97f5720f
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-26 09:06:37 -0500

    Add course code

--- avis.js
+++ avis.js
@@ -7,7 +7,18 @@ export function ajoutListenersAvis() {
      piecesElements[i].addEventListener("click", async function (event) {
  
         const id = event.target.dataset.id;
-        fetch("http://localhost:8081/pieces/" + id + "/avis");
+        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+        const avis = await reponse.json();
+        const pieceElement = event.target.parentElement;
+
+        const avisElement = document.createElement("p");
+        for (let i = 0; i < avis.length; i++) {
+            avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+        }
+
+
+
+pieceElement.appendChild(avisElement);
  
      });
  
