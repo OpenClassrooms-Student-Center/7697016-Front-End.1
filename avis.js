e397825881d991007662b8c5fe7d1283345856e6
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-26 09:55:20 -0500

    Add code from course

--- avis.js
+++ avis.js
@@ -20,6 +20,26 @@ export function ajoutListenersAvis() {
      });
  
     }
- 
  }
  
+ export function ajoutListenerEnvoyerAvis() {
+    const formulaireAvis = document.querySelector(".formulaire-avis");
+    formulaireAvis.addEventListener("submit", function (event) {
+    event.preventDefault();
+    // Création de l’objet du nouvel avis.
+    const avis = {
+        pieceId: event.target.querySelector("[name=piece-id]").value,
+        utilisateur: event.target.querySelector("[name=utilisateur").value,
+        commentaire: event.target.querySelector("[name=commentaire]").value,
+    };
+    // Création de la charge utile au format JSON
+    const chargeUtile = JSON.stringify(avis);
+    // Appel de la fonction fetch avec toutes les informations nécessaires
+    fetch("http://localhost:8081/avis", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: chargeUtile
+    });
+    });
+    
+ }
--- pieces.js
+++ pieces.js
@@ -1,8 +1,10 @@
-import { ajoutListenersAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
 
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('http://localhost:8081/pieces/');
 const pieces = await reponse.json();
+// on appel la fonction pour ajouter le listener au formulaire
+ajoutListenerEnvoyerAvis()
 
 function genererPieces(pieces){
     for (let i = 0; i < pieces.length; i++) {
