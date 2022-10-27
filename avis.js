fac25b05770ae28ad856633fbc372b21be84ee37
aurelien-oc-mentorship <65898532+aurelien-oc-mentorship@users.noreply.github.com>
2022-10-27 11:52:43 -0500

    fix format document

--- avis.js
+++ avis.js
@@ -1,84 +1,84 @@
 export function ajoutListenersAvis() {
 
     const piecesElements = document.querySelectorAll(".fiches article button");
- 
-    for (let i = 0; i < piecesElements.length; i++) {
- 
-     piecesElements[i].addEventListener("click", async function (event) {
- 
-        const id = event.target.dataset.id;
-        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
-        const avis = await reponse.json();
-        window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
-        const pieceElement = event.target.parentElement;
-        afficherAvis(pieceElement, avis)
-     });
- 
-    }
- }
- 
- export function afficherAvis(pieceElement, avis){
-    const avisElement = document.createElement("p");
-        for (let i = 0; i < avis.length; i++) {
-            avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
-        }
-        pieceElement.appendChild(avisElement);
- }
 
- export function ajoutListenerEnvoyerAvis() {
+    for (let i = 0; i < piecesElements.length; i++) {
+
+        piecesElements[i].addEventListener("click", async function (event) {
+
+            const id = event.target.dataset.id;
+            const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+            const avis = await reponse.json();
+            window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
+            const pieceElement = event.target.parentElement;
+            afficherAvis(pieceElement, avis)
+        });
+
+    }
+}
+
+export function afficherAvis(pieceElement, avis) {
+    const avisElement = document.createElement("p");
+    for (let i = 0; i < avis.length; i++) {
+        avisElement.innerHTML += `<b>${avis[i].utilisateur}:</b> ${avis[i].commentaire} <br>`;
+    }
+    pieceElement.appendChild(avisElement);
+}
+
+export function ajoutListenerEnvoyerAvis() {
     const formulaireAvis = document.querySelector(".formulaire-avis");
     formulaireAvis.addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Création de l’objet du nouvel avis.
-    const avis = {
-        pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
-        utilisateur: event.target.querySelector("[name=utilisateur").value,
-        commentaire: event.target.querySelector("[name=commentaire]").value,
-        nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
+        event.preventDefault();
+        // Création de l’objet du nouvel avis.
+        const avis = {
+            pieceId: parseInt(event.target.querySelector("[name=piece-id]").value),
+            utilisateur: event.target.querySelector("[name=utilisateur").value,
+            commentaire: event.target.querySelector("[name=commentaire]").value,
+            nbEtoiles: parseInt(event.target.querySelector("[name=nbEtoiles]").value)
+        };
+        // Création de la charge utile au format JSON
+        const chargeUtile = JSON.stringify(avis);
+        // Appel de la fonction fetch avec toutes les informations nécessaires
+        fetch("http://localhost:8081/avis", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: chargeUtile
+        });
+    });
+
+}
+
+export async function afficherGraphiqueAvis() {
+    // Calcul du nombre total de commentaires par quantité d'étoiles attribuées
+    const avis = await fetch("http://localhost:8081/avis").then(avis => avis.json());
+    const nb_commentaires = [0, 0, 0, 0, 0];
+
+    for (let commentaire of avis) {
+        nb_commentaires[commentaire.nbEtoiles - 1]++;
+    }
+    // Légende qui s'affichera sur la gauche à côté de la barre horizontale
+    const labels = ["5", "4", "3", "2", "1"];
+    // Données et personnalisation du graphique
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: "Étoiles attribuées",
+            data: nb_commentaires.reverse(),
+            backgroundColor: "rgba(255, 230, 0, 1)", // couleur jaune
+        }],
     };
-    // Création de la charge utile au format JSON
-    const chargeUtile = JSON.stringify(avis);
-    // Appel de la fonction fetch avec toutes les informations nécessaires
-    fetch("http://localhost:8081/avis", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: chargeUtile
-    });
-    });
-    
- }
-
- export async function afficherGraphiqueAvis(){
-     // Calcul du nombre total de commentaires par quantité d'étoiles attribuées
- const avis = await fetch("http://localhost:8081/avis").then(avis => avis.json());
- const nb_commentaires = [0, 0, 0, 0, 0];
-
- for (let commentaire of avis) {
-     nb_commentaires[commentaire.nbEtoiles - 1]++;
- }
- // Légende qui s'affichera sur la gauche à côté de la barre horizontale
- const labels = ["5", "4", "3", "2", "1"];
- // Données et personnalisation du graphique
- const data = {
-     labels: labels,
-     datasets: [{
-     label: "Étoiles attribuées",
-     data: nb_commentaires.reverse(),
-    backgroundColor: "rgba(255, 230, 0, 1)", // couleur jaune
- }],
- };
- // Objet de configuration final
- const config = {
-     type: "bar",
-     data: data,
-     options: {
-         indexAxis: "y",
-     },
- };
- // Rendu du graphique dans l'élément canvas
- const graphiqueAvis = new Chart(
-     document.querySelector("#graphique-avis"),
-     config,
- );
- }
+    // Objet de configuration final
+    const config = {
+        type: "bar",
+        data: data,
+        options: {
+            indexAxis: "y",
+        },
+    };
+    // Rendu du graphique dans l'élément canvas
+    const graphiqueAvis = new Chart(
+        document.querySelector("#graphique-avis"),
+        config,
+    );
+}
 
