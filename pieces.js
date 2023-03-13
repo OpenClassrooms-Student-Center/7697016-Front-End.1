be8271485be025e0a8fbbf5793c3e5ad715a0866
chantpep <102247532+Chantal-Pepin@users.noreply.github.com>
2023-03-13 14:35:08 -0400

    Correction aux deux éléments ajoutés dans la fiche :  document.createElement....
    J'avais oublié de mettre document avant createElement...

--- pieces.js
+++ pieces.js
@@ -6,15 +6,22 @@ const article = pieces[0];
 const imageElement = document.createElement("img");
 imageElement.src = article.image;
 
-
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 
 const prixElement = document.createElement("p");
-
 prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
 const categorieElement = document.createElement("p");
 categorieElement.innerText = article.categorie ?? "(Aucune categorie)";
+
+// Ajout des nouveaux élément pour la fiche de produit
+
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
+
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
 
 const sectionFiches = document.querySelector(".fiches");
 
@@ -22,5 +29,9 @@ sectionFiches.appendChild(imageElement);
 sectionFiches.appendChild(nomElement);
 sectionFiches.appendChild(prixElement);
 sectionFiches.appendChild(categorieElement);
+
+// Ajout de ces deux nouveaux éléments
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
 
 
