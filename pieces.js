0da9df8e35a42dc42c6796c0b841221f1f03a980
chantpep <102247532+Chantal-Pepin@users.noreply.github.com>
2023-03-16 17:23:32 -0400

    JS : Ajout de la boucle For et insertions des éléments à l'intérieur. "
    This reverts commit 4fa33be0908b4dc90f3813449a465298072ee5e9.

--- pieces.js
+++ pieces.js
@@ -2,38 +2,36 @@
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
 
-for (let i = 0; i < pieces.length; i++) {
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
 
-    const article = pieces[i];
-    // Récupération de l'élément du DOM qui accueillera les fiches
-    const sectionFiches = document.querySelector(".fiches");
-    // Création d’une balise dédiée à une pièce automobile
-    const pieceElement = document.createElement("article");
-    // Création des balises 
-    const imageElement = document.createElement("img");
-    imageElement.src = article.image;
-    const nomElement = document.createElement("h2");
-    nomElement.innerText = article.nom;
-    const prixElement = document.createElement("p");
-    prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-    const descriptionElement = document.createElement("p");
-    descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
-    const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-    
-    // On rattache la balise article a la section Fiches
-    sectionFiches.appendChild(pieceElement);
-    // On rattache l’image à pieceElement (la balise article)
-    pieceElement.appendChild(imageElement);
-    pieceElement.appendChild(nomElement);
-    pieceElement.appendChild(prixElement);
-    pieceElement.appendChild(categorieElement);
-    //Ajout des éléments au DOM pour l'exercice
-    pieceElement.appendChild(descriptionElement);
-    pieceElement.appendChild(stockElement);
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
 
- }
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie ?? "(Aucune categorie)";
+
+// Ajout des nouveaux élément pour la fiche de produit
+
+const descriptionElement = document.createElement("p");
+descriptionElement.innerText = article.description ?? "(Pas de description pour le moment)";
+
+const disponibiliteElement = document.createElement("p");
+disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+const sectionFiches = document.querySelector(".fiches");
+
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
+
+// Ajout de ces deux nouveaux éléments
+sectionFiches.appendChild(descriptionElement);
+sectionFiches.appendChild(disponibiliteElement);
 
 
