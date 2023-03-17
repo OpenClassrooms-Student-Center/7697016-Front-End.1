865c987bd7c12ea556ea4b32d5b9ebf62b34a747
chantpep <102247532+Chantaloup@users.noreply.github.com>
2023-03-17 11:19:32 -0400

    Ajout des éléments de chaque fiche
    Création dynamique des éléments de chaque fiche

---
+++ pieces.js
@@ -0,0 +1,23 @@
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch('pieces-autos.json');
+const pieces = await reponse.json();
+
+// Créations des éléments dynamiquement
+
+const article = pieces[0];
+const imageElement = document.createElement("img");
+imageElement.src = article.image;
+const nomElement = document.createElement("h2");
+nomElement.innerText = article.nom;
+const prixElement = document.createElement("p");
+prixElement.innerText = `Prix: ${article.prix} €`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie;
+
+// Insertion des éléments dans le parent qui a la classe ".fiches"
+
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches.appendChild(imageElement);
+sectionFiches.appendChild(nomElement);
+sectionFiches.appendChild(prixElement);
+sectionFiches.appendChild(categorieElement);
