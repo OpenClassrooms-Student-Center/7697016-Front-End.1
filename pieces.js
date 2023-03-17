164f5236ef73d8c141693b5debaa016042e265de
chantpep <102247532+Chantaloup@users.noreply.github.com>
2023-03-17 11:42:35 -0400

    Ajout Opérateurs ternaire et nullish

--- pieces.js
+++ pieces.js
@@ -10,9 +10,9 @@ imageElement.src = article.image;
 const nomElement = document.createElement("h2");
 nomElement.innerText = article.nom;
 const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} €`;
+prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie;
+categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
 
 // Insertion des éléments dans le parent qui a la classe ".fiches"
 
