652e569032b058872f184ce0e99f07ad3301fac3
chantpep <102247532+Chantaloup@users.noreply.github.com>
2023-03-22 17:58:08 -0400

    Diverses mise à jour de pieces.js
    Effacer l'écran et les informations de produits
    Ajout de la fonction genererPièces() pour générer une nouvelle page avec les nouvelles informations
    Ajouter l'écouteur d'événement qui permet de sortir les informations selon le filtre.

--- pieces.js
+++ pieces.js
@@ -1,40 +1,51 @@
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
-const pieces = await reponse.json();
+const pieces = await reponse.json()
 
-// Créations des éléments dynamiquement
+//Fonction qui génère toute la page
 
-for (let i = 0; i < pieces.length; i++) {
+function genererPieces(pieces) {
+    for (let i = 0; i < pieces.length; i++) {
+        const article = pieces[i];
+        const sectionFiches = document.querySelector(".fiches");        
+        // La piece principale 
+        const pieceElement = document.createElement("article");
+        // Images
+        const imageElement = document.createElement("img");
+        imageElement.src = article.image;
+        // Nom
+        const nomElement = document.createElement("h2");
+        nomElement.innerText = article.nom;
+        // Prix
+        const prixElement = document.createElement("p");
+        prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+        // Catégorie
+        const categorieElement = document.createElement("p");
+        categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+        // Description
+        const descriptionElement = document.createElement("p");
+        descriptionElement.innerText = article.description ?? "(Pas de description pour le moment.)";
+        // Disponibilité
+        const disponibiliteElement = document.createElement("p");
+        disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+        
+        // on rattache tout le reste à pieceElement
+       
+        sectionFiches.appendChild(pieceElement);
+        pieceElement.appendChild(imageElement);
+        pieceElement.appendChild(nomElement);
+        pieceElement.appendChild(prixElement);
+        pieceElement.appendChild(categorieElement);
+        pieceElement.appendChild(descriptionElement);
+        pieceElement.appendChild(disponibiliteElement);
 
-const article = pieces[i];
-const sectionFiches = document.querySelector(".fiches");
-// nouvel élément pour balise article liée à automobile
-const pieceElement = document.createElement("article");
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText = article.description ?? "(Pas de description pour le moment.)";
-const disponibiliteElement = document.createElement("p");
-disponibiliteElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
-
-// Insertion des éléments dans le parent qui a la classe ".fiches"
-
-// insertion du nouvel élément article à la section Fiche
-sectionFiches.appendChild(pieceElement);
-// on rattache tout le reste à pieceElement
-pieceElement.appendChild(imageElement);
-pieceElement.appendChild(nomElement);
-pieceElement.appendChild(prixElement);
-pieceElement.appendChild(categorieElement);
-pieceElement.appendChild(descriptionElement);
-pieceElement.appendChild(disponibiliteElement);
+        //document.body.appendChild(pieceElement);
+    }
 }
+
+// Efface le contenu de la balise body et donc l’écran
+//document.querySelector(".fiches").innerHTML = '';
+genererPieces(pieces)
 
 const boutonTrier = document.querySelector(".btn-trier");
 boutonTrier.addEventListener("click", () => {
@@ -126,3 +137,12 @@ document.querySelector('.abordable').appendChild(abordablesElements);
 // Ajout des éléments disponibles dans le bloc ayant la classe .disponible
 
 document.querySelector('.disponible').appendChild(disponiblesElements);
+
+const prixMax = document.querySelector('#prixMax');
+prixMax.addEventListener('input', () => {
+    const lesPiecesFiltrees = pieces.filter((piece) => {
+        return piece.prix <= prixMax.value;
+    })
+    document.querySelector(".fiches").innerHTML = '';
+    genererPieces(lesPiecesFiltrees)
+})
