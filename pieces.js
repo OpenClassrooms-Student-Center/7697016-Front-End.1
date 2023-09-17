d2f6a9d169685b853a469a88809f9b069aedc087
Ambrosie CAZIMIRA <a.cazimira@gmail.com>
2023-09-17 19:39:12 +0200

    extraire les elements de mon fichier json

---
+++ pieces.js
@@ -0,0 +1,30 @@
+const response = await fetch("pieces-autos.json");
+const pieces = await response.json();
+
+for (let i = 0; i < pieces.length; i++) {
+	const article = pieces[i];
+
+	const imageElement = document.createElement("img");
+	imageElement.src = article.image;
+
+	const nomElement = document.createElement("h2");
+	nomElement.innerText = article.nom;
+
+	const prixElement = document.createElement("p");
+	prixElement.innerText = `Prix: ${article.prix} € (${
+		article.prix < 35 ? "€" : "€€€"
+	}) `;
+
+	const categorieElement = document.createElement("p");
+	categorieElement.innerText = article.categorie ?? "";
+
+	const descriptionElement = document.createElement("p");
+	descriptionElement.innerText = article.description;
+
+	const sectionFiches = document.querySelector(".fiches");
+	sectionFiches.appendChild(imageElement);
+	sectionFiches.appendChild(nomElement);
+	sectionFiches.appendChild(prixElement);
+	sectionFiches.appendChild(categorieElement);
+	sectionFiches.appendChild(descriptionElement);
+}
