070246bc4973e4fc9c598406f2761199a488d4dc
Chantaloup <102247532+Chantaloup@users.noreply.github.com>
2023-10-25 03:12:25 -0400

    Projet terminé !

--- pieces.js
+++ pieces.js
@@ -1,9 +1,18 @@
-import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis, afficherAvis, afficherGraphiqueAvis } from "./avis.js";
 
-
-// Récupération des pièces depuis le fichier JSON
-const reponse = await fetch('http://localhost:8081/pieces');
-const pieces = await reponse.json()
+//Récupération des pièces eventuellement stockées dans le localStorage
+let pieces = window.localStorage.getItem('pieces');
+if (pieces === null){
+   // Récupération des pièces depuis l'API
+   const reponse = await fetch('http://localhost:8081/pieces/');
+   pieces = await reponse.json();
+   // Transformation des pièces en JSON
+   const valeurPieces = JSON.stringify(pieces);
+   // Stockage des informations dans le localStorage
+   window.localStorage.setItem("pieces", valeurPieces);
+}else{
+   pieces = JSON.parse(pieces);
+}
 
 ajoutListenerEnvoyerAvis();
 
@@ -59,7 +68,19 @@ function genererPieces(pieces) {
 
 // Efface le contenu de la balise body et donc l’écran
 //document.querySelector(".fiches").innerHTML = '';
-genererPieces(pieces)
+genererPieces(pieces);
+
+
+for(let i = 0; i < pieces.length; i++){
+    const id = pieces[i].id;
+    const avisJSON = window.localStorage.getItem(`avis-piece-${id}`);
+    const avis = JSON.parse(avisJSON);
+
+    if(avis !== null){
+        const pieceElement = document.querySelector(`article[data-id="${id}"]`);
+        afficherAvis(pieceElement, avis)
+    }
+}
 
 const boutonTrier = document.querySelector(".btn-trier");
 boutonTrier.addEventListener("click", () => {
@@ -165,3 +186,11 @@ prixMax.addEventListener('input', () => {
     genererPieces(lesPiecesFiltrees)
 })
 
+
+// Ajout du listener pour mettre à jour des données du localStorage
+const boutonMettreAJour = document.querySelector(".btn-maj");
+boutonMettreAJour.addEventListener("click", function () {
+  window.localStorage.removeItem("pieces");
+});
+
+await afficherGraphiqueAvis();
